3692eb9764cd8619f1f90e95e6d1fa6a609a3a37
Rizal Hillman <rizal@imastudio.co.id>
2018-08-15 20:29:27 +0700

    first commit

--- CoreDataLagi/ViewController.swift
+++ CoreDataLagi/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: context)
+//        let newUser = NSManagedObject(entity: entity!, insertInto: context)
+//        newUser.setValue("admin", forKey: "username")
+//        newUser.setValue("admin5", forKey: "password")
+//        newUser.setValue("20", forKey: "age")
+//
+//        newUser.setValue("asep admin", forKey: "fullname")
+//        do {
+//            try context.save()
+//            print("Berhasil")
+//        } catch {
+//            print("Failed saving")
+//        }
+        
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        request.predicate = NSPredicate(format: "username = %@", "admin")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                let username = data.value(forKey: "username") as! String
+                let password = data.value(forKey: "password") as! String
+                
+                if username == "admin" && password == "admin5" {
+                    print("Login berhasil")
+                } else {
+                    print("Login gagal")
+                }
+            }
+            
+        } catch {
+            
+            print("Failed")
+        }
     }
 
     override func didReceiveMemoryWarning() {
